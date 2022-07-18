bcbcd250c3c1e8a3aea31698231a953ea930812b
OsaruElaiho <Osaruelaiho@gmail.com>
2022-07-18 17:23:51 -0500

    Login & Signup

--- flashcards-ui/src/components/App/App.jsx
+++ flashcards-ui/src/components/App/App.jsx
@@ -1,10 +1,51 @@
+import * as React from "react"
 import './App.css'
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+import LoginPage from '../LoginPage/LoginPage'
+import RegistrationPage from '../RegistrationPage/RegistrationPage'
+import NotFound from '../NotFound/NotFound'
+import apiClient from "../../../services/apiClient"
+import { AuthContextProvider, useAuthContext } from "../../../contexts/auth"
+import { useEffect,  } from 'react'
 
-function App() {
+export default function AppContainer () {
   return (
-    <div className="App">
-    </div>
+    <AuthContextProvider>
+      <App/>
+    </AuthContextProvider>
   )
 }
 
-export default App
+function App() {
+  const {user, setUser, error, setError} = useAuthContext();
+  
+  useEffect(() => {
+    const fetchUser = async () => {
+      const {data, err} = await apiClient.fetchUserFromToken()
+      if (data) setUser(data.user)
+      if (err) setError(err)
+    }
+
+    const token = localStorage.getItem("flashcard_token");
+    if(token) {
+      apiClient.setToken(token)
+      fetchUser()
+    }
+  }, [])
+
+  return (
+    <div className="App">
+      <React.Fragment>{
+        <BrowserRouter>
+          {/* <Navbar/> */}
+          <Routes>
+            {/* <Route path="/" element={<Landing/>} /> */}
+            <Route path="/login" element={<LoginPage/>} />
+            <Route path="/register" element={<RegistrationPage/>}/>
+            <Route path="*" element={<NotFound/>}/>
+            </Routes>
+        </BrowserRouter>
+      }</React.Fragment>
+    </div>
+  )
+}
