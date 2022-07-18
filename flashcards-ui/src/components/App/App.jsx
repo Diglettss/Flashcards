3facc9200ba25c78ad05d6db810698ec11a66270
Daniel Jonas <02danieljonas@gmail.com>
2022-07-18 15:01:11 -0400

    redirection for CreateSetPage and MySetPage

--- flashcards-ui/src/components/App/App.jsx
+++ flashcards-ui/src/components/App/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "../Navbar/Navbar";
 import Landing from "../Landing/Landing";
 import LoginPage from "../LoginPage/LoginPage";
 import RegistrationPage from "../RegistrationPage/RegistrationPage";
+import MySetPage from "../MySetPage/MySetPage.jsx";
+import CreateSetPage from "../CreateSetPage/CreateSetPage.jsx";
 import NotFound from "../NotFound/NotFound.jsx";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { AuthContextProvider, useAuthContext } from "../contexts/Auth";
@@ -25,6 +27,8 @@ function App() {
                     <Route path="/" element={<Landing />} />
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/register" element={<RegistrationPage />} />
+                    <Route path="/mysets" element={<MySetPage />} />
+                    <Route path="/create" element={<CreateSetPage />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
             </BrowserRouter>
--- flashcards-ui/src/components/contexts/Auth.jsx
+++ flashcards-ui/src/components/contexts/Auth.jsx
@@ -3,7 +3,16 @@ import { createContext, useContext, useEffect, useState } from "react";
 const AuthContext = createContext(null);
 
 export const AuthContextProvider = ({ children }) => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(
+        localStorage.getItem("isLoggedIn") || false
+    );
+
+    //this is called every time isLoggedIn is changed
+    useEffect(() => {
+        localStorage.setItem("isLoggedIn", isLoggedIn);
+    }, [isLoggedIn]);
+
+    
     const authValue = { isLoggedIn, setIsLoggedIn };
 
     return (
