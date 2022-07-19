d17619966d4dfe0e7dcec8e5f2d8615c4b8cbea1
Daniel Jonas <02danieljonas@gmail.com>
2022-07-19 10:43:35 -0400

    info passed to FlashcardRow is shown

--- flashcards-ui/src/components/App/App.jsx
+++ flashcards-ui/src/components/App/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Flashcard from "../Flashcard/Flashcard.jsx";
 
 function App() {
   return (
     <div className="App">
+      <Flashcard/>
     </div>
   )
 }
+
+
 
 export default App
