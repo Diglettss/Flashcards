36c08c6d03586b3f1b48b16c8bb2fb60fdcf70fd
OsaruElaiho <Osaruelaiho@gmail.com>
2022-07-20 10:36:29 -0500

    landing page styling

--- flashcards-ui/src/components/Landing/Landing.jsx
+++ flashcards-ui/src/components/Landing/Landing.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import "./Landing.css"
-import "./study"
+import React from "react";
+import "./Landing.css";
 
 export default function Landing() {
   return (
     <div className="landing-page">
-        <div className="hero">
-            <img className="hero-img" src="http://codepath-lifetracker.surge.sh/static/media/smartwatch-screen-digital-device.e2983a85.svg" alt="hero-img" />
-            <div className="cta">
-                <h1>Flashy</h1>
-                <p>Helping you study...blah blah</p>
-            </div>
+      <div className="hero">
+        <div className="hero-img">
+          <img
+            className="study-img"
+            src={"/src/study_hero.jpeg"}
+            alt="study-img"
+          />
         </div>
+        <div className="message">
+          <h1>Flashy</h1>
+          <p>Helping you study...blah blah</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
