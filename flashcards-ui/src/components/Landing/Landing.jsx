37c281f62d9bdfaed5b8957b8bbc5f0b6ece6d8a
OsaruElaiho <Osaruelaiho@gmail.com>
2022-07-20 15:29:29 -0500

    styling

--- flashcards-ui/src/components/Landing/Landing.jsx
+++ flashcards-ui/src/components/Landing/Landing.jsx
@@ -6,15 +6,20 @@ export default function Landing() {
     <div className="landing-page">
       <div className="hero">
         <div className="hero-img">
-          <img
+          {/* <img
             className="study-img"
             src={"/src/study_hero.jpeg"}
+            alt="study-img"
+          /> */}
+          <img
+            className="study-img"
+            src={"https://via.placeholder.com/350x150"}
             alt="study-img"
           />
         </div>
         <div className="message">
           <h1>Flashy</h1>
-          <p>Helping you study...blah blah</p>
+          <p>Helping you study...</p>
         </div>
       </div>
     </div>
