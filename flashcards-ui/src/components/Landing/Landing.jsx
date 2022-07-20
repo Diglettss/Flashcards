6f5e60d73c9d4a75e43c93f6e97a8ca438c45d62
OsaruElaiho <Osaruelaiho@gmail.com>
2022-07-20 00:47:49 -0500

    Landing-Page (not final)

--- flashcards-ui/src/components/Landing/Landing.jsx
+++ flashcards-ui/src/components/Landing/Landing.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import "./Landing.css"
+import "./study"
 
 export default function Landing() {
   return (
-    <div>Landing</div>
+    <div className="landing-page">
+        <div className="hero">
+            <img className="hero-img" src="http://codepath-lifetracker.surge.sh/static/media/smartwatch-screen-digital-device.e2983a85.svg" alt="hero-img" />
+            <div className="cta">
+                <h1>Flashy</h1>
+                <p>Helping you study...blah blah</p>
+            </div>
+        </div>
+    </div>
   )
 }
