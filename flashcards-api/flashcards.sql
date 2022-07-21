3de16317e99750173b97775153470244447b5ac7
OsaruElaiho <Osaruelaiho@gmail.com>
2022-07-21 11:44:55 -0500

    initialized backend

---
+++ flashcards-api/flashcards.sql
@@ -0,0 +1,7 @@
+ \echo 'Delete and recreate flashcards db?' \prompt 'Return for yes or control-C to cancel > ' answer
+DROP DATABASE flashcards;
+
+
+CREATE DATABASE flashcards;
+
+\connect flashcards \i flashcards_schema.sql
