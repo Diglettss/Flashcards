eadb7d953348753e40b6b9060ab3ccca7f474b3f
Daniel Jonas <02danieljonas@gmail.com>
2022-07-21 22:27:08 -0400

    cleaned up AuthContextProvider

--- flashcards-ui/contexts/auth.jsx
+++ flashcards-ui/contexts/auth.jsx
@@ -1,100 +1,9 @@
 import { createContext, useState, useContext, useEffect } from "react";
 import apiClient from "../services/apiClient";
-import { LoremIpsum } from "lorem-ipsum";
 
 const AuthContext = createContext(null);
 
 export const AuthContextProvider = ({ children }) => {
-    //TODO put this in its own context file
-    const [showSettingsModal, setShowSettingsModal] = useState(false);
-    const [info, setInfo] = useState({
-        title: "Title of set",
-        description: "description of set",
-        setId: 1,
-        flashcard: [
-            {
-                term: "term1",
-                id: "0",
-                definition: "Lorem ipsum dolor.",
-                selected: true,
-            },
-            {
-                term: "term2",
-                id: "1",
-                definition: "Lorem ipsum consectetur sint dolores consequatur!",
-            },
-            {
-                term: "term3",
-                id: "2",
-                definition:
-                    "Lorem ipsum condimentum leo in libero pulvinar pellentesque ac eget risus. Aenean rutrum molestie elit, vitae ultricies est facilisis sed. Fusce vitae massa tortor!",
-            },
-            {
-                term: "term4",
-                id: "3",
-                definition:
-                    "Lorem ipsum odio et sodales. Curabitur malesuada luctus dolor ac eleifend. Aliquam erat volutpat. Vivamus tincidunt eu odio at efficitur. Pellentesque lacinia eleifend..",
-            },
-            {
-                term: "term5",
-                id: "4",
-                definition:
-                    "Lorem ipsum  vel aliquam ante. Aenean quis suscipit neque, id maximus risus. Donec a enim vel turpis tincidunt porta a vitae nulla. Nulla in urna efficitur, fringilla felis eu, euismod ligula. Suspendisse bibendum orci vitae finibus lobortis. Phasellus sed sollicitudin tellus. Ut imperdiet mauris quis tempus venenatis. Ut vel rutrum velit.",
-            },
-        ],
-    });
-    info.flashcard.forEach((e) => {
-        e.selected = true; //Math.random() < 0.5;
-    });
-    const lorem = new LoremIpsum({
-        sentencesPerParagraph: {
-            max: 8,
-            min: 4,
-        },
-        wordsPerSentence: {
-            max: 16,
-            min: 4,
-        },
-    });
-    lorem.generateWords(71);
-
-    const randomSet = (numOfSet) => {
-        let set = [];
-
-        const create = () => {
-            let flashcardss = [];
-            let randomInt = Math.floor(Math.random() * 13) + 5;
-            for (let k = 0; k < randomInt; k++) {
-                flashcardss.push({
-                    term: `term${k}`,
-                    id: k,
-                    definition: lorem.generateWords(
-                        Math.floor(Math.random() * 22) + 10
-                    ),
-                    selected: true,
-                });
-            }
-            return flashcardss;
-        };
-
-        for (let i = 0; i < numOfSet; i++) {
-            set.push({
-                title: "Title of set",
-                description: lorem.generateWords(
-                    Math.floor(Math.random() * 22) + 10
-                ),
-                setId: i,
-                flashcard: create(),
-            });
-        }
-        return set;
-    };
-
-    const [mySets, setmySets] = useState(randomSet(3));
-    console.log(mySets);
-
-    //This is to be changeable by the user
-    const [defaultFlashcardState, setDefaultFlashcardState] = useState(true);
     const [user, setUser] = useState({});
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -208,13 +117,6 @@ export const AuthContextProvider = ({ children }) => {
         logoutUser,
         isLoggedIn,
         setIsLoggedIn,
-        showSettingsModal,
-        setShowSettingsModal,
-        info,
-        setInfo,
-        defaultFlashcardState,
-        setDefaultFlashcardState,
-        mySets,
     };
 
     return (
