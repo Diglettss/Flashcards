36f72dca1a4e7575aedbde17686d1c5218708eb3
Daniel Jonas <02danieljonas@gmail.com>
2022-07-23 15:41:25 -0400

    searching doesn't work but sorting is fixed

--- flashcards-ui/src/components/MySetPage/MySetPage.jsx
+++ flashcards-ui/src/components/MySetPage/MySetPage.jsx
@@ -7,6 +7,9 @@ export default function MySetPage() {
     "This takes the varibale mySets fron contexts/flashcard and loops it through the Set component";
     const { mySets } = useFlashcardContext();
     const [sortBy, setSortBy] = useState("Newest");
+    const [searchValue, setSearchValue] = useState("");
+
+    const [filteredMySets, setFilteredMySets] = useState([...mySets]);
 
     // const navigate = useNavigate();
 
@@ -15,8 +18,6 @@ export default function MySetPage() {
     //         navigate("/login");
     //     }
     // }, [isLoggedIn]);
-
-    //TODO maybe useEffect
 
     const sortByNewestDate = (array) => {
         array.sort((a, b) => b.date - a.date);
@@ -40,27 +41,70 @@ export default function MySetPage() {
 
     //TODO bug with useEffect, the sort only takes effect after changing the valu
     useEffect(() => {
-        console.log("Change")
-        if (sortBy == "Newest") {
-            sortByNewestDate(mySets);
-        } else if (sortBy == "Oldest") {
-            sortByOldestDate(mySets);
-        } else if (sortBy == "A-Z") {
-            sortByAlphabeticalOrder(mySets);
-        } else if (sortBy == "Z-A") {
-            sortByReverseAlphabeticalOrder(mySets);
-        } else{
-            sortByNumOfFlashcards(mySets)
-        }
-    }, [sortBy]);
+        // console.log(JSON.stringify(mySets[0].title.includes(searchValue)))
+        // console.log(JSON.stringify(mySets[0].title))
+        //if searchValue is in the title or description set it to a variable
+        const fillteredSetByTitleDescription = mySets.filter((e) => {
+            if (e.title.includes(searchValue)) {
+                return true;
+            }
+            if (e.description.includes(searchValue)) {
+                return true;
+            }
+            console.log(e.title, e.description, "false")
+            return false;
+        });
 
+        // if the searchValue is empty set the variable to the full mySets
+        // if (searchValue == "") {
+        //     fillteredSetByTitleDescription = mySets;
+        // }
+
+        //set that filtered variable in a useState value
+        setFilteredMySets(
+            mySets.filter((e) => {
+                if (e.title.includes(searchValue)) {
+                    return true;
+                }
+                // if (e.description.includes(searchValue)) {
+                //     return true;
+                // }
+                console.log(e.title, e.description, "false")
+                return false;
+            })
+        );
+        console.log(filteredMySets.length, mySets.length, fillteredSetByTitleDescription.length)
+
+        
+        //sorts the sets
+        if (sortBy == "Newest") {
+            sortByNewestDate(fillteredSetByTitleDescription);
+        } else if (sortBy == "Oldest") {
+            sortByOldestDate(fillteredSetByTitleDescription);
+        } else if (sortBy == "A-Z") {
+            sortByAlphabeticalOrder(fillteredSetByTitleDescription);
+        } else if (sortBy == "Z-A") {
+            sortByReverseAlphabeticalOrder(fillteredSetByTitleDescription);
+        } else {
+            sortByNumOfFlashcards(fillteredSetByTitleDescription);
+        }
+        //This is needed to rerender the sets
+        setFilteredMySets([...fillteredSetByTitleDescription]);
+    }, [sortBy, searchValue]);
     return (
         <div className="my-sets-page">
             <div>
                 <label htmlFor="search-input">User's sets</label>
             </div>
             {sortBy}
-            <input className="search-input" type="text" />
+            <input
+                className="search-input"
+                type="text"
+                value={searchValue}
+                onChange={(e) => {
+                    setSearchValue(e.target.value);
+                }}
+            />
             <select
                 className="drop-down"
                 onChange={(e) => {
@@ -75,7 +119,7 @@ export default function MySetPage() {
                 <option>Z-A</option>
                 <option># of flashcards</option>
             </select>
-            {mySets.map((e, idx) => (
+            {filteredMySets.map((e, idx) => (
                 <Set set={e} key={idx} />
             ))}
         </div>
