bf31a29cbc07af7663883f72820e51ce5e4cae7a
Daniel Jonas <02danieljonas@gmail.com>
2022-07-26 11:48:33 -0400

    fixed last_ping

--- flashcards-api/flashcards_schema.sql
+++ flashcards-api/flashcards_schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE users(
     last_name       TEXT NOT NULL,
     email           TEXT NOT NULL UNIQUE CHECK(POSITION('@' IN email) > 1),
     created_at      TIMESTAMP NOT NULL DEFAULT NOW(),
-    last_ping       TIMESTAMP NOT NULL
+    last_ping       TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE FlashcardSets( 
