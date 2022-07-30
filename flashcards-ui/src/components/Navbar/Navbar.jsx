55176b93cfea4c4aba4ada5f4a466f446d97db58
OsaruElaiho <Osaruelaiho@gmail.com>
2022-07-30 10:26:18 -0500

    Navbar (not final)

--- flashcards-ui/src/components/Navbar/Navbar.jsx
+++ flashcards-ui/src/components/Navbar/Navbar.jsx
@@ -1,61 +1,102 @@
-import React, { useState, ReactNode } from "react";
+import React, { useState } from "react";
 import {
     Box,
-    Flex,
-    Avatar,
-    HStack,
-    // Link,
-    IconButton,
     Button,
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
-    MenuDivider,
-    useDisclosure,
+    ButtonGroup,
+    Container,
+    Flex,
+    HStack,
+    IconButton,
+    useBreakpointValue,
     useColorModeValue,
-    Stack,
-  } from '@chakra-ui/react';
+    Icon,
+} from "@chakra-ui/react";
+import { BsLightningChargeFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import NavLinks from "../NavLinks/NavLinks.jsx";
 import Searchbar from "../Searchbar/Searchbar.jsx";
 import UserProfile from "../UserProfile/UserProfile.jsx";
-import "./Navbar.css";
+import { useNavigate } from "react-router-dom";
+// import "./Navbar.css";
 import { useAuthContext } from "../../../contexts/auth";
 
 function Navbar() {
     const { user } = useAuthContext();
     const [isProfileOpen, setIsProfileOpen] = useState(false);
+    const navigate = useNavigate();
     return (
-        <nav className="Navbar">
-            <div className="links">
-                <Link to="/" className="logo">
-                    <img
-                        src="https://via.placeholder.com/64"
-                        alt="website logo"
-                        className="logo"
-                    />
-                </Link>
-                <NavLinks />
-            </div>
-            <Searchbar />
-            <div className="user-profile-img">
-                <img
-                    src="https://via.placeholder.com/600x600"
-                    alt="profile image"
-                    className={
-                        user.email ? "profile-image" : "profile-image hidden"
-                    }
+        <Box
+            as="nav"
+            bg="green.300"
+            boxShadow={useColorModeValue("lg", "sm-dark")}
+            pt={6}
+            pb={2}
+        >
+            <HStack spacing={20} alignItems={"center"} pl={50}>
+                <Icon
+                    as={BsLightningChargeFill}
                     onClick={() => {
-                        setIsProfileOpen(!isProfileOpen);
+                        navigate("/");
                     }}
                 />
-            </div>
-            <div className="user-modal">
-                {isProfileOpen ? <UserProfile /> : null}
-            </div>
-        </nav>
+                <HStack
+                    as={"nav"}
+                    spacing={4}
+                    display={{ base: "none", md: "flex" }}
+                >
+                    <Button
+                        variant="ghost"
+                        size="lg"
+                        onClick={() => {
+                            navigate("/login");
+                        }}
+                    >
+                        Login
+                    </Button>
+                    <Button
+                        variant="ghost"
+                        size="lg"
+                        onClick={() => {
+                            navigate("/login");
+                        }}
+                    >
+                        Register
+                    </Button>
+                </HStack>
+            </HStack>
+        </Box>
     );
+
+    // return (
+    //     <nav className="Navbar">
+    //         <div className="links">
+    //             <Link to="/" className="logo">
+    //                 <img
+    //                     src="https://via.placeholder.com/64"
+    //                     alt="website logo"
+    //                     className="logo"
+    //                 />
+    //             </Link>
+    //             <NavLinks />
+    //         </div>
+    //         <Searchbar />
+    //         <div className="user-profile-img">
+    //             <img
+    //                 src="https://via.placeholder.com/600x600"
+    //                 alt="profile image"
+    //                 className={
+    //                     user.email ? "profile-image" : "profile-image hidden"
+    //                 }
+    //                 onClick={() => {
+    //                     setIsProfileOpen(!isProfileOpen);
+    //                 }}
+    //             />
+    //         </div>
+    //         <div className="user-modal">
+    //             {isProfileOpen ? <UserProfile /> : null}
+    //         </div>
+    //     </nav>
+    // );
 }
 
 export default Navbar;
