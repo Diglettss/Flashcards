b4af4a43c4e9434d9962673f491792cda7e920ed
02danieljonas <02danieljonas@gmail.com>
2022-08-09 05:54:14 -0400

    cleaned up package.json

--- flashcards-ui/constants.js
+++ flashcards-ui/constants.js
@@ -1,5 +1,5 @@
 export const PRODUCTION_API_BASE_URL="https://flashi-api.herokuapp.com/"
 export const DEVELOPMENT_API_BASE_URL="http://localhost:3001"
-const API_BASE_URL=PRODUCTION_API_BASE_URL
+const API_BASE_URL=DEVELOPMENT_API_BASE_URL
 
 export default API_BASE_URL
