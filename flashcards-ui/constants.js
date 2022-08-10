64720a92b94147af9015430a15b505b942cd5326
02danieljonas <02danieljonas@gmail.com>
2022-08-10 09:59:18 -0400

    created dynamic API_BASE_URL

--- flashcards-ui/constants.js
+++ flashcards-ui/constants.js
@@ -1,5 +1,15 @@
-export const PRODUCTION_API_BASE_URL="https://flashi-api.herokuapp.com/"
-export const DEVELOPMENT_API_BASE_URL="http://localhost:3001"
-const API_BASE_URL=DEVELOPMENT_API_BASE_URL
+export const PRODUCTION_API_BASE_URL = "https://flashi-api.herokuapp.com/";
+export const DEVELOPMENT_API_BASE_URL = "http://localhost:3001";
 
-export default API_BASE_URL
+let isDev = null;
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+    isDev = true;
+    // dev code
+} else {
+    isDev = false;
+    // production code
+}
+
+const API_BASE_URL = isDev ? DEVELOPMENT_API_BASE_URL : PRODUCTION_API_BASE_URL;
+
+export default API_BASE_URL;
